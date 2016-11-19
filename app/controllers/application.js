63e9d0773a17206f9781ba11f60fa766132070d9
Pedro Rio <pedrorio@gmail.com>
2016-11-19 19:02:17 +0000

    Add an action to the attribute to make DDAU compatible

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+const {
+  Controller,
+  get
+} = Ember;
+
+export default Controller.extend({
+
+  actions: {
+    setName(name) {
+      let model = get(this, 'model');
+      model.set('name', name);
+    },
+
+    setAge(age) {
+      let model = get(this, 'model');
+      model.set('age', age);
+    }
+  }
 });
