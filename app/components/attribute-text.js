f4a15598e405d1eb5456bad4cf734c0d803108fc
Pedro Rio <pedrorio@gmail.com>
2016-11-19 21:00:12 +0000

    Make the component retrieve the label, help and placeholder automatically
    using the model name and attribute name passed

--- app/components/attribute-text.js
+++ app/components/attribute-text.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  Component,
+  inject: { service },
+  computed,
+} = Ember;
+
+export default Component.extend({
+  i18n: service(),
+
+  attribute: '',
+
+  getTranslation(property) {
+    return this.get('i18n').t(`model.${this.get('modelName')}.${this.get('attribute')}.${property}`);
+  },
+
+  modelName: computed.alias('model.constructor.modelName'),
+
+  helpText: computed('i18n.locale', function() {
+    return this.getTranslation('help').toString();
+  }),
+
+  label: computed('i18n.locale', function() {
+    return this.getTranslation('label').toString();
+  }),
+
+  hasHelp: computed('helpText', function() {
+    return this.get('helpText').length > 0;
+  }),
+
+  placeholder: computed('i18n.locale', function() {
+    return this.getTranslation('placeholder').toString();
+  })
+
 });
